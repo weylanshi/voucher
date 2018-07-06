2f675409bcb34a1b7ce6492a225eea6a8d6a9556
fishOfLake <1147286587@qq.com>
2018-07-06 17:09:21 +0800

    添加合同列表接口

--- src/main/java/com/u8/sum/SumApplication.java
+++ src/main/java/com/u8/sum/SumApplication.java
@@ -2,6 +2,7 @@ package com.u8.sum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SumApplication {
