c991fa819ef12ced8b04348a6f530b7e2c002e6e
weylan <weylan@126.com>
2018-07-08 15:06:19 +0800

    添加登陆功能

--- src/main/java/com/u8/sum/controller/LoginController.java
+++ src/main/java/com/u8/sum/controller/LoginController.java
@@ -3,23 +3,25 @@ package com.u8.sum.controller;
 import com.u8.sum.consts.Result;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-    @Value("user.userName")
+    @Value("${user.userName}")
     private String userName;
 
-    @Value("user.password")
+    @Value("${user.password}")
     private String password;
 
     @RequestMapping("/api/login")
-    public Result login(String userName,String password) {
-        if(StringUtils.isBlank(userName)||StringUtils.isBlank(password)){
+    public Result login( String username, String password) {
+        if(StringUtils.isBlank(username)||StringUtils.isBlank(password)){
             return Result.ERROR("用户名或密码为空");
         }
-        if(!password.trim().toLowerCase().equals(password)){
+        if(!username.equals(this.userName)||!password.trim().toLowerCase().equals(this.password)){
             return Result.ERROR("用户名或密码错误");
         }
         return Result.OK();
--- src/test/java/com/u8/sum/SumApplicationTests.java
+++ src/test/java/com/u8/sum/SumApplicationTests.java
@@ -1,7 +1,6 @@
 package com.u8.sum;
 
-import com.u8.sum.domain.report.CmExecuteBill;
-import com.u8.sum.repository.report.CmExecuteBillRepository;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +13,13 @@ import java.util.List;
 @SpringBootTest
 public class SumApplicationTests {
 
-	@Autowired
-	private  CmExecuteBillRepository cmExecuteBillRepository;
+
 
 	@Test
 	public void contextLoads() {
 	}
 
-	@Test
-	public void testFindAll(){
-		List<CmExecuteBill> all = cmExecuteBillRepository.findAll();
-		System.out.println(all);
-	}
+
 
 
 
